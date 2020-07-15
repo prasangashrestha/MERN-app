217b6633c5ff32b4c33b7b3137cbbfcd2be06a2d
Prasanga <101890972@student.swin.edu.au>
2020-07-15 19:36:24 +1000

    add users list component

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-import "UserList.css";
+import "./UserList.css";
+import UserItem from "./UserItem";
 
-const UserList = () => {};
+const UserList = ({items}) => {
+  if (items.length === 0) {
+    return (
+      <div className='center'>
+        <h2>No users found</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UserList from "../components/UserList";
 
-const Users = (props) => {
-  return <div>User Works</div>;
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Prasanga Shrestha",
+      image:
+        "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
