5055e0a715bf089e07a9d5ea1610be9bbec6b9bd
Prasanga <101890972@student.swin.edu.au>
2020-07-15 19:10:36 +1000

    setup files

---
+++ src/user/components/UserList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "UserList.css";
+
+const UserList = () => {};
+
+export default UserList;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = (props) => {
+  return <div>User Works</div>;
+};
+
+export default Users;
