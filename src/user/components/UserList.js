072bbfc7b57770c4598321d6f180ed37589397be
Prasanga <101890972@student.swin.edu.au>
2020-07-16 02:15:26 +1000

    add avatar and card ui components

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./UserList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserList = ({items}) => {
   if (items.length === 0) {
     return (
       <div className='center'>
-        <h2>No users found</h2>
+        <Card>No users found</Card>
       </div>
     );
   }
