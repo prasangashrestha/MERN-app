56cf1615b528a05943e6303ba32e312a19d508f6
Prasanga <101890972@student.swin.edu.au>
2020-07-16 14:00:55 +1000

    add brackdop for mobile view navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {useState, Fragment} from "react";
 
 import "./MainNavigation.css";
 import {Link} from "react-router-dom";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/BackDrop";
 
 const MainNavigation = ({children}) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
-    <MainHeader>
-      <button className='main-navigation__menu-btn'>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className='main-navigation__title'>
-        <Link to='/'>Your Places</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>Your Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
